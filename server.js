93727bfd8e513e146d7d0cd1bd3f9e80c1b4f14e
Natannan <natannanz16@gmail.com>
2024-02-02 23:58:55 +0300

    Updated

--- server.js
+++ server.js
@@ -10,13 +10,11 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const accountSid = 'AC07a8f23cbe8a35a85c5fd9550cdd1e79';
-const authToken =  '81c841c3c256db0bfa740bac269ed8d0';
-const twilioPhoneNumber = '+16176065334';
+const accountSid = '';
+const authToken =  '';
+const twilioPhoneNumber = '+';
 const userPhoneNumber = '+251973073279';
-// AC07a8f23cbe8a35a85c5fd9550cdd1e79
-// 81c841c3c256db0bfa740bac269ed8d0
-//+16176065334
+
 
 const client = twilio(accountSid, authToken);
 let isSMSSentLow = false;
