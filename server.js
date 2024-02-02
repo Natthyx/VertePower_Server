724be47baadda3ee84a734a777b18e7554722447
Natannan <natannanz16@gmail.com>
2024-02-02 23:57:11 +0300

    Updated

--- server.js
+++ server.js
@@ -5,20 +5,23 @@ const path = require('path');
 const dot_env = require('dotenv').config()
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const accountSid = process.env.SID;
-const authToken = process.env.TOKEN;
-const twilioPhoneNumber = '+15854548309';
+const accountSid = 'AC07a8f23cbe8a35a85c5fd9550cdd1e79';
+const authToken =  '81c841c3c256db0bfa740bac269ed8d0';
+const twilioPhoneNumber = '+16176065334';
 const userPhoneNumber = '+251973073279';
+// AC07a8f23cbe8a35a85c5fd9550cdd1e79
+// 81c841c3c256db0bfa740bac269ed8d0
+//+16176065334
 
 const client = twilio(accountSid, authToken);
 let isSMSSentLow = false;
 let isSMSSentHigh = false;
-let isLEDTurnedOn = false;
+// let isLEDTurnedOn = false;
 
 app.post('/soil-moisture', (req, res) => {
   const moisture = req.body.moisture;
@@ -28,8 +31,8 @@ app.post('/soil-moisture', (req, res) => {
     isSMSSentLow = true;
     isSMSSentHigh = false;
 
-  } else if (moisture > 35 && !isSMSSentHigh) {
-    sendSMS('Soil moisture is above 35%. Turn off the pump.');
+  } else if (moisture > 23 && !isSMSSentHigh) {
+    sendSMS('Soil moisture is above 60%. Turn off the pump.');
     isSMSSentHigh = true;
     isSMSSentLow = false;
   }
@@ -68,5 +71,5 @@ function sendSMS(message) {
 }
 
 app.listen(port, () => {
-    console.log(`Server is running on http://172.20.10.7:${port}`);
+    console.log(`Server is running on http://172.20.10.9:${port}`);
 });
